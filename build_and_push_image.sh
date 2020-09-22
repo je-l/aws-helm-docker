9597ffa673e3c3090a76474dc33673222be23853
je-l <je-l@users.noreply.github.com>
2020-09-22 16:07:34 +0300

    Also push latest image tag

--- build_and_push_image.sh
+++ build_and_push_image.sh
@@ -2,17 +2,20 @@
 
 set -euxo pipefail
 
+export $(go run fetch_versions.go)
 
 CREATED_DATE=$(date --rfc-3339=date)
-export $(go run fetch_versions.go)
-IMAGE_NAME=jljljl/aws-helm:$CREATED_DATE
+NEW_IMAGE=jljljl/aws-helm:$CREATED_DATE
+LATEST_IMAGE=jljljl/aws-helm:latest
 
 docker build \
   --build-arg CREATED_DATE="$CREATED_DATE" \
   --build-arg KUBECTL_VERSION="$KUBECTL_VERSION" \
   --build-arg HELM_VERSION="$HELM_VERSION" \
   --build-arg AWS_CLI_VERSION="$AWS_CLI_VERSION" \
-  -t "$IMAGE_NAME" \
+  -t "$NEW_IMAGE" \
   .
 
-docker push $IMAGE_NAME
+docker tag "$NEW_IMAGE" "$LATEST_IMAGE"
+docker push "$NEW_IMAGE"
+docker push "$LATEST_IMAGE"
